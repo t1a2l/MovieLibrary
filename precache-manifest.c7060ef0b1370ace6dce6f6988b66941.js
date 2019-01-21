9c8fc6db452e57808c34866de166c254673390c6
t1a2l <talrofeh@gmail.com>
2019-01-22 00:35:35 +0200

    Updates

---
+++ precache-manifest.c7060ef0b1370ace6dce6f6988b66941.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a7b533a2da000666b82e",
+    "url": "/static/js/main.a7b533a2.chunk.js"
+  },
+  {
+    "revision": "a08fd91f719f652851c9",
+    "url": "/static/js/1.a08fd91f.chunk.js"
+  },
+  {
+    "revision": "a7b533a2da000666b82e",
+    "url": "/static/css/main.2c7e8970.chunk.css"
+  },
+  {
+    "revision": "c05ac3712b6c24abeb05d334604b2fa1",
+    "url": "/index.html"
+  }
+];
